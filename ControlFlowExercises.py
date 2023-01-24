80a982c5640e4e24e38a654fa6fec98cd5227a3a
James Farrell <me@ell.com>
2023-01-24 10:59:31 +0000

    answered the questions

---
+++ ControlFlowExercises.py
@@ -0,0 +1,114 @@
+print("\nQ1a\n")
+# Q1a: Print only the first 5 numbers in this list
+x = [2, 5, 4, 87, 34, 2, 1, 31, 103, 99]
+
+# A1a:
+print(x[:5])
+
+
+print("\nQ1b\n")
+# Q1b: Now print only the even numbers in this list (the elements that are themselves even)
+x = [2, 5, 4, 87, 34, 2, 1, 31, 103, 99]
+
+# A1b:
+print([a for a in x if a % 2 ==0])
+
+
+print("\nQ1c\n")
+# Q1c: Now only print the even numbers up to the fifth element in the list (e.g. 2, 4, 34)
+x = [2, 5, 4, 87, 34, 2, 1, 31, 103, 99]
+
+# A1c:
+y = x[:5]
+y = [a for a in x if a % 2 ==0]
+print(y)
+
+# -------------------------------------------------------------------------------------- #
+
+print("\nQ2a\n")
+# Q2a: from the list of names, create another list that consists of only the first letters of each first name
+# e.g. ["Alan Turing", "Leonardo Fibonacci"] -> ["A", "L"]
+names = ["Alan Turing", "Leonardo Fibonacci", "Katherine Johnson", "Annie Easley", "Terence Tao"]
+
+# A2a:
+first = []
+for name in names:
+    first.append(name[0])
+print(first)
+
+
+print("\nQ2b\n")
+# Q2b: from the list of names, create another list that consists of only the index of the space in the string
+# HINT: use your_string.index("substring")
+names = ["Alan Turing", "Leonardo Fibonacci", "Katherine Johnson", "Annie Easley", "Terence Tao"]
+
+# A2b:
+index_space = []
+for name in names:
+    index_space.append(name.index(' '))
+print(index_space)
+
+
+
+print("\nQ2c\n")
+# Q2c: from the list of names, create another list that consists of the first and last initial of each individual
+names = ["Alan Turing", "Leonardo Fibonacci", "Katherine Johnson", "Annie Easley", "Terence Tao"]
+
+# A2c:
+first_last = []
+for name in names:
+    first_last.append(name[0])
+    first_last.append(name[-1])
+
+print(first_last) 
+
+
+print("\nQ3a\n")
+# Q3a: Here is a list of lists, print only the lists which have no duplicates
+# Hint: This can be easily done by using sets as a set does not contain duplicates
+list_of_lists = [[1,5,7,3,44,4,1],
+                 ["A", "B", "C"],
+                 ["Hi", "Hello", "Ciao", "By", "Goodbye", "Ciao"],
+                 ["one", "Two", "Three", "Four"]]
+
+
+# A3a:
+list_set = []
+for list in list_of_lists:
+    list_set.append(set(list))
+
+print(list_set)
+# -------------------------------------------------------------------------------------- #
+
+print("\nQ4a\n")
+# Q4a: Using a while loop, ask the user to input a number greater than 100, if they enter anything else,
+# get them to enter again (and repeat until the conditions are satisfied). Finally print the number that
+# they entered
+
+# A4a:
+print('type a number greater than 100')
+user_input = int(input())
+try:
+    while user_input < 100:
+        print('put in a number greater than 100')
+except ValueError:
+    print('Please type a number')
+
+
+print("\nQ4b\n")
+# Q4b: Continue this code and print "prime" if the number is a prime number and "not prime" otherwise
+
+# A4b:
+prime_checker = False
+for a in range(2, user_input):
+    if (user_input % a) == 0:
+        prime_checker = True
+if prime_checker:
+    print('Not prime')
+else:
+    print('Prime')
+
+
+
+
+
