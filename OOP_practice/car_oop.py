6ad05f297f29c03cba48549c5b12ccab5dc0d9dc
James Farrell <me@ell.com>
2023-01-25 16:37:36 +0000

    car running smoothly

--- OOP_practice/car_oop.py
+++ OOP_practice/car_oop.py
@@ -1,54 +1,62 @@
 class Car:
-    def __init__(self, colour='White', style='SUV', doors = 5, make= 'Volkswagen' , model='Tiguan'): #add 5 arguments to instantiate
+    def __init__(self, make= 'Volkswagen',  model='Tiguan', colour='White', style='SUV', doors = 5,): #add 5 arguments to instantiate
         self.colour = colour
         self.style = style
         self.doors = doors
         self.make = make
         self.model = model
         self._acceleration = 0
-        self._direction = ''
-        self._brake = 0
+        self._direction = '' # single underscore to say it is private 
+        self.__brake = 0 #add in the double underscore so you can't edit by variable
+        self.status = ''
 
     def acceleration(self, rate):
         '''add in the speed of acceleration in mph for this method'''
         if not type(rate) is float:           #checking data type
             raise TypeError('Only integers allowed')
         self._acceleration = rate           #applying the 
-        
+
     def check_acceleration(self):
-        '''returns the accelration speed'''
+        '''returns the acceleration speed'''
         print(f'your {self.make} {self.model} will acclerate from 0-60 in: {self._acceleration} seconds!')
 
     def brake(self, distance):
         if not type(distance) is int:
             raise TypeError('only numbers allowed')
-        self._brake = distance
+        self.__brake = distance
+        
 
     def braking_distance(self):
         '''implement the braking distance in metres'''
-        print(f'The braking distance of your {self.make} {self.model} is {self._brake} metres')
+        print(f'The braking distance of your {self.make} {self.model} is {self.__brake} metres')
 
     def steer(self):
         '''choose a direction in which you want to steer North, East, South, West'''
+        self.status = 'driving'
         directions = ['north', 'east', 'south', 'west']
         print('Please choose a direction in which you want to steer? \n North, East, South or West?')
-        
-        try:
-            direction = str(input()).lower()
-            if direction in directions:
-                self._direction = direction
-        except ValueError('please enter a direction, North, East, South or West...') as ve:
-            print(ve)
-                
-        
+        check_direct = True
+        while check_direct:
+            try:
+                direction = str(input()).lower()
+                if direction in directions:
+                    self._direction = direction
+                    check_direct = False
+                else:
+                    print('please enter North, East, South or West')
+            except ValueError as ve:
+                print(ve)
+            
     
     def direction_car_facing(self):
-        print(f'{self.make} {self.model} is currently facing {self._direction}')
+        print(f'{self.make} {self.model} is currently facing {self._direction} and is {self.status}')
+
     def park(self):
-        pass
+        self.status = 'parked'
+
 
 class Door(Car):
-    def __init__(self, colour, open_style, lock_type, handle):
+    def __init__(self, colour, open_style = 'Regular Doors', lock_type='automatic lock with key', handle= 'trigger'):
         super().__init__(colour)
         self.open_style = open_style
         self.lock_type = lock_type
--- OOP_practice/run_car_oop.py
+++ OOP_practice/run_car_oop.py
@@ -1,19 +1,37 @@
 from car_oop import Car
 
 
-def create_car(name):
-    car = Car(name)
+def create_car(name= 'BMW', *args):
+    car = Car(name, *args)
+    car.acceleration(5.9)
+    car.brake(40)
     return car
 
+def create_toyota_hatchback():
+    toyota = Car('Red', 'Hatchback', 3, 'Toyota', 'Celica')
+    toyota.acceleration(4.1)
+    toyota.brake(40)
+    toyota.__brake = 32 # this will stop the variable from being changed as protected with '__'
+    toyota.steer()
+    
+def return_information(obj):
+    obj.braking_distance()
+    obj.check_acceleration()
+    obj.direction_car_facing()
 
-toyota = Car('Red', 'Hatchback', 3, 'Toyota', 'Celica')
-toyota.acceleration(4.1)
-toyota.check_acceleration()
-toyota.brake(40)
-toyota.braking_distance()
-toyota.steer()
-toyota.direction_car_facing()
 
 if __name__ == '__main__':
-    vw = create_car('gary')
-    print(vw.model)
+    gary_the_car = create_car() # creating the car gary, which is an object
+    sheila_the_car = create_car('Ford', 'Mustang', 'Red', 'Sports Car', 5) # using *args
+    sheila_the_car.acceleration(3.1)
+    print(gary_the_car.make)
+    print(gary_the_car.model) 
+    print(sheila_the_car) #returns the object info
+    toyota_the_first = create_toyota_hatchback()
+    toyota_the_second = create_toyota_hatchback()
+    toyota_the_third = create_toyota_hatchback()
+    print(toyota_the_first, ': first toyota')
+    print(toyota_the_second, ': second toyota')
+    print(toyota_the_third, ': third toyota')
+    return_information(sheila_the_car)
+    return_information(gary_the_car)
